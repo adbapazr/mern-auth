fc9004cb22831029cbcd755ec151412e4dd0053f
Adamu Bappale <adbapazr@gmail.com>
2024-10-15 23:36:00 +0100

    connect to DB

--- api/index.js
+++ api/index.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+dotenv.config();
+
+mongoose
+.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+})
+.catch((error) => {
+    console.log(error);
+});
 
 const app = express();
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Server is running on port 3000');
 });
