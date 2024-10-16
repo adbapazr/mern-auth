ab9be62c2d8192c56c9943338cb7d02d5130b879
Adamu Bappale <adbapazr@gmail.com>
2024-10-16 07:29:46 +0100

    create sign up api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+import bcryptjs from 'bcryptjs';
 dotenv.config();
 
 mongoose
@@ -15,8 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(process.env.PORT, () => {
   console.log('Server is running on port 3000');
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
